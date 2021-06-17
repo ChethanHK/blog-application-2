bc4ba26db71353aaec99ee2799e8ec90ebe7d1c1
ChethanHK <chethanhk2000@gmail.com>
2021-06-17 17:04:55 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+  path('', views.PostList.as_view(), name="home"),
+  path('<slug:slug>/', views.PostList.as_view(), name="post_detail"),
+  path('<int:post_id>/share/',
+           views.post_share, name='post_share'),
+]
